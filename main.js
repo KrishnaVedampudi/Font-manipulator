6c69de3b8a7b757e2676b0d1d1ed987d54e758e1
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2020-12-14 10:11:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 550);
+    video.position(005, 350);
+
+    canvas = createCanvas(500, 500);
+    canvas.position(1000, 350);
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() 
+{
+    console.log('poseNet is Initialized');
+}
+noseX=0;
+noseY=0;
+leftWrist=0;
+rightWrist=0;
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+
+        leftWrist = results[0].pose.leftWrist.x;
+        rightWrist = results[0].pose.rightWrist.x;
+        console.log('nose X= ' + noseX + 'noseY = ' + noseY);        
+    }
+}
+function draw()
+{
+    background('#969A97');
+    fill('red');
+    stroke('black');    
+}
